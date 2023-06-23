085c87abc8e2a75dc5412fa238ae40228f4493a7
highbuyer <cyhighbuyery@gmail.com>
2023-06-23 21:22:19 +0800

    update go version and deps

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -1,8 +1,12 @@
 package redis
 
 import (
-	"github.com/highbuyer/proxypool/pool"
+	"github.com/gomodule/redigo/redis"
+	"log"
+	"time"
 )
+
+var pool *redis.Pool
 
 type RedisClient interface {
 	GetConn() (conn redis.Conn, err error)
@@ -10,18 +14,74 @@ type RedisClient interface {
 }
 
 type redisClient struct {
-	pool pool.RedisClient // 修改此处为公共模块中的接口类型。
+	pool *redis.Pool
 }
 
 func NewRedisClient(addr, password string) RedisClient {
-	client := &redisClient{pool.NewRedisPool(addr, password)}
-	return client // 返回新构建的客户端对象即可。
+	pool := &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			return newConnection(addr, password)
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+		// 这里根据需要进行配置...
+	}
+
+	client := &redisClient{pool}
+	return client
 }
+
 func (c *redisClient) GetConn() (conn redis.Conn, err error) {
 	conn = c.pool.Get()
+
+	if _, err = conn.Do("PING"); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
 	return conn, nil
 }
 
 func (c *redisClient) Close() {
 	c.pool.Close()
+}
+
+// 内部实现细节，不需要对外暴露
+func newConnection(addr, password string) (conn redis.Conn, err error) {
+	c, err := redigo.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	if len(password) > 0 {
+		_, authErr := c.Do("AUTH", password)
+		if authErr != nil {
+			log.Fatal(authErr)
+			return nil, authErr
+		}
+	}
+
+	return c, nil
+}
+
+// 内部实现细节，不需要对外暴露
+func init() {
+	addr := "localhost:6379" // 修改为实际地址和端口号
+	password := ""           // 如果有密码，请在这里输入密码
+
+	pool = &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			return newConnection(addr, password)
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+		MaxIdle:     10,
+		MaxActive:   100,
+		IdleTimeout: (60 * time.Second),
+	}
 }
--- internal/redis/redis_test.go
+++ internal/redis/redis_test.go
@@ -1,26 +1,75 @@
-// internal/redis/redis_test.go
-
-package redis
+package redistest
 
 import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"github.com/highbuyer/proxypool/redis" // 替换为您的实际项目导入路径
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestSetAndGet(t *testing.T) {
-	client := NewRedisClient("127.0.0.1:6379", "")
+func TestConnectRedis(t *testing.T) {
+	client := NewRedisClient("localhost:6379", "")
 	conn, err := client.GetConn()
-	assert.Nil(t, err)
+	if err != nil {
+		t.Errorf("Expected nil error, but got %v instead", err)
+	}
 
-	key := "test_key"
-	value := "test_value"
+	defer conn.Close()
 
-	_, err = conn.Do("SET", key, value)
-	assert.Nil(t, err)
+	value1 := "test_value1"
+	key1 := "test_key1"
 
-	reply, _ := redis.String(conn.Do("GET", key))
-	assert.Equal(t, value, reply)
+	setErr := setValue(conn, key1, value1)
+	if setErr != nil {
+		t.Errorf("Expected no errors when setting value to key. Got %v instead.", setErr.Error())
+		return
+	}
 
-	client.Close()
+	resultVal, err := getValue(conn, key1)
+	if resultVal != value1 || err != nil {
+		t.Errorf("Expected the same value as was set in Redis for key=%s. Got %v,%v respectively.", key1,
+			resultVal, err.Error())
+		return
+	}
+
+	// 使用带密码的客户端连接到 Redis 服务器进行授权操作和数据读写。
+	authClient := NewRedisClient("localhost:6379", "password")
+	defer authClient.Close()
+
+	conn2, authErr := authClient.GetConn()
+	if authErr != nil {
+		t.Errorf("Expected no errors when connecting with authorization. Got %v instead.", authErr.Error())
+		return
+	}
+
+	defer conn2.Close()
+
+	value2 := "test_value2"
+	key2 := "test_key2"
+
+	setAuthError := setValue(conn2, key2, value2)
+	if setAuthError != nil {
+		t.Errorf("Expected no errors when setting a value with authorization. Got %v instead.", setAuthError.Error())
+		return
+	}
+
+	authVal, autherr := getValue(conn2, key2)
+	if authVal != value2 || autherr != nil {
+		//t.Errorf(fmt.Sprintf("expected: %v but got: %v (error: %v)", expectedValue, resultValue, err))
+		t.Errorf(fmt.Sprintf("expected: %v but got: %v (error: %v)", value2, authVal, err))
+		return
+	}
+}
+func setValue(conn redis.Conn, key string, value string) error {
+	_, err := conn.Do("SET", key, value)
+	return err
+}
+
+func getValue(conn redis.Conn, key string) (string, error) {
+	valueBytes, err := redis.Bytes(conn.Do("GET", key))
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s", valueBytes), nil
+
 }
